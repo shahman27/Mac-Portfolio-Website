155f87d5d64ee727f161eb341ba1298490382cf9
Dhruv Shah <dhruvshah38@gmail.com>
2023-12-25 22:42:39 -0500

    added logo and time to menubar working on dock

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import MenuBar from './components/MenuBar';
+import Dock from './components/Dock';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <MenuBar />
-      <Routes>
-        
-      </Routes>
+      <Dock />
     </Router>
   );
 }
--- portfolio-website/src/components/Dock.js
+++ portfolio-website/src/components/Dock.js
@@ -1,1 +1,12 @@
 import React from "react";
+import "../styles/dock.css";
+
+function Dock() {
+    return (
+        <div className="dock">
+            <p>hi</p>
+        </div>
+    )
+}
+
+export default Dock;
