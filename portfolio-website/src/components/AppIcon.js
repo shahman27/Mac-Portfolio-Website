79310d161d4d5111da0dcc1dbb86035d4e2a7de3
Dhruv Shah <dhruvshah38@gmail.com>
2024-03-31 21:45:22 -0400

    added app icon to the dock

---
+++ portfolio-website/src/components/AppIcon.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/app-icon.css";
+
+function AppIcon(props) {
+    return (
+        <div className="app-icon">
+            <h> hello </h>
+        </div>
+    )
+}
+
+export default AppIcon;
--- portfolio-website/src/components/Dock.js
+++ portfolio-website/src/components/Dock.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "../styles/dock.css";
+import AppIcon from "./AppIcon";
 
 function Dock() {
     return (
         <div className="dock">
-
+            <AppIcon />
+            <AppIcon />
+            <AppIcon />
+            <AppIcon />
+            <AppIcon />
         </div>
     )
 }
